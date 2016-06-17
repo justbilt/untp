82b7d91cd2f42addfa4407126205e698edb82cf8
justbilt <wangbilt@gmail.com>
2016-06-17 23:01:51 +0800

    dump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'untp',
-    version = '1.0.3',
+    version = '1.0.4',
     keywords = ('untp', 'texturepacker'),
     description = 'A command line tool to split TexturePacker publish file.',
     license = 'MIT License',
