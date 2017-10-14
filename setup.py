96edbe59491ddb6c2ebd2c0f38abc0861d9f9de5
justbilt <wangbilt@gmail.com>
2017-10-14 19:01:12 +0800

    refine: setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,28 @@ from setuptools import setup, find_packages
 setup(
     name = 'untp',
     version = '1.1.0',
-    keywords = ('untp', 'texturepacker'),
     description = 'A command line tool to split TexturePacker published files.',
+    url = 'https://github.com/justbilt/untp',
+    author = 'justbilt',
+    author_email = 'wangbilt@gmail.com',
     license = 'MIT License',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],    
+    keywords = 'untp texturepacker cocos',
     install_requires = [
         'Pillow',
         'parse'
     ],
-    url = 'https://github.com/justbilt/untp',
-    author = 'justbilt',
-    author_email = 'wangbilt@gmail.com',
     packages = find_packages("src"),
     package_dir = {'':'src'},
     entry_points = {
