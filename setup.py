f7cb756c506a376cc82b6645537936431004f43b
justbilt <wangbilt@gmail.com>
2017-10-14 19:28:39 +0800

    bomp version to 1.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'untp',
-    version = '1.1.0',
+    version = '1.1.1',
     description = 'A command line tool to split TexturePacker published files.',
     url = 'https://github.com/justbilt/untp',
     author = 'justbilt',
